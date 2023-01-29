3a50fa972b35435886a0e6e43d926b749c6560e3
Skorzi <lobarev_danil@mail.ru>
2023-01-29 20:21:05 +0500

    Скролл во время пребывания в бургерменю

--- frontend/front_divani/src/blocks/MainBlock.js
+++ frontend/front_divani/src/blocks/MainBlock.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import BurgerMenu from '../components/burgermenu/BurgerMenu'
 import Header from '../components/header/Header'
 
 export default function MainBlock() {
+  
   return (
     <div className='main'>
         <Header/>
+        <BurgerMenu/>
     </div>
   )
 }
--- frontend/front_divani/src/components/header/Header.js
+++ frontend/front_divani/src/components/header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import BurgerMenu from './BurgerMenu'
+import BurgerMenu from '../burgermenu/BurgerMenu'
+import BurgerButton from '../burgermenu/BurgerButton'
 import LinksCategory from './LinksCategory'
 import '../../static/css/header.css'
 export default function Header() {
 
     const list_of_links = [
-        {name: 'Каталог мебели', href: '#'},
-        {name: 'Доставка и оплата', href: '#'},
-        {name: 'О нас', href: '#'},
-        {name: 'Связаться с нами', href: '#'},
-        {name: 'Статьи о мебели', href: '#'},
+        { name: 'Каталог мебели', href: '#' },
+        { name: 'Доставка и оплата', href: '#' },
+        { name: 'О нас', href: '#' },
+        { name: 'Связаться с нами', href: '#' },
+        { name: 'Статьи о мебели', href: '#' },
     ]
 
 
@@ -18,8 +19,8 @@ export default function Header() {
             <div className='header__block'>
                 <div className='header__content'>
                     <div className='header__logo'>DIVANI_TUTTI</div>
-                    <LinksCategory links={list_of_links} classnameUl={'header__links'} classnameLi={'header__category'}/>
-                    <div className='header__burger'><BurgerMenu categories={list_of_links}/></div>
+                    <LinksCategory links={list_of_links} classnameUl={'header__links'} classnameLi={'header__category'} />
+                    <div className='header__burger'><BurgerButton /></div>
                 </div>
             </div>
         </div>
